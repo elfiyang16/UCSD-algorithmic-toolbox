82386cf709601c82cd5f59d5b6049ca8d0fcd905
Elfi Yang <elfiyang16@gmail.com>
2020-06-11 14:09:32 +0100

    feat: week 5

--- week5_dynamic_programming1/4_longest_common_subsequence_of_two_sequences/lcs2.py
+++ week5_dynamic_programming1/4_longest_common_subsequence_of_two_sequences/lcs2.py
@@ -1,10 +1,16 @@
 #Uses python3
 
 import sys
-
 def lcs2(a, b):
-    #write your code here
-    return min(len(a), len(b))
+	T = [[0]*(len(a)+1) for _ in range(len(b)+1)]
+	for i in range(1,len(a)+1):
+		for j in range(1,len(b)+1):
+			if a[i-1] == b[j-1]:
+				T[j][i] = 1+ T[j-1][i-1]
+			else:
+				T[j][i] = max(T[j][i-1],T[j-1][i])
+	return T[j][i]
+
 
 if __name__ == '__main__':
     input = sys.stdin.read()
